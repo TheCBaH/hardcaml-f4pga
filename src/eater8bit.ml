e5a0843762ce22e9d00540a308b5a6a556e42eb4
Vladimir N. Silyaev <vsilyaev@gmail.com>
2023-05-08 04:34:02 +0000

    fixup! First version

--- src/eater8bit.ml
+++ src/eater8bit.ml
@@ -43,9 +43,59 @@ let register_test =
   set inputs.w_en;
   cycles 2;
   set inputs.reset;
-  set inputs.w_en;
+  cycles 2;
+  clear inputs.reset;
   for i = 4 to 8 do
     data i;
     cycles 1
   done;
-  Hardcaml_waveterm.Waveform.print ~display_height:16 ~display_width:80 ~wave_width:0 waves
+  cycles 1;
+  Hardcaml_waveterm.Waveform.print ~display_height:18 ~display_width:80 ~wave_width:0 waves
+
+module Alu = struct
+  let bits = 8
+
+  module I = struct
+    type 'a t = { op : 'a; a : 'a; [@bits bits] b : 'a [@bits bits] } [@@deriving sexp_of, hardcaml]
+  end
+
+  module O = struct
+    type 'a t = { data : 'a; [@bits bits] carry : 'a; zero : 'a } [@@deriving sexp_of, hardcaml]
+  end
+
+  let create scope i =
+    ignore scope;
+    let open Signal in
+    let a = succ bits |> Signal.uresize i.I.a in
+    let b = succ bits |> Signal.uresize i.I.b in
+    let add = a +: b in
+    let sub = a -: b in
+    let result = mux2 i.I.op add sub in
+    let carry = msb result in
+    let data = lsbs result in
+    let zero = data ==:. 0 in
+    { O.data; carry; zero }
+end
+
+let alu_test =
+  let scope = Scope.create ~flatten_design:true () in
+  let module Simulator = Cyclesim.With_interface (Alu.I) (Alu.O) in
+  let waves, sim =
+    Alu.create scope |> Simulator.create ~config:Cyclesim.Config.trace_all |> Hardcaml_waveterm.Waveform.create
+  in
+  let inputs = Cyclesim.inputs sim in
+  let set w v = w := Bits.of_int ~width:Alu.bits v in
+  let alu op a b =
+    set inputs.a a;
+    set inputs.b b;
+    inputs.op := Bits.of_int ~width:1 op;
+    Cyclesim.cycle sim
+  in
+  alu 1 0x2 0x4;
+  alu 0 0x6 0x1;
+  alu 0 0x3 0x3;
+  alu 0 0x1 0x3;
+  alu 1 0x70 0x80;
+  alu 1 0xF0 0x10;
+  alu 1 0xF0 0x20;
+  Hardcaml_waveterm.Waveform.print ~display_height:18 ~display_width:80 ~wave_width:1 waves
